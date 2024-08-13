b6be2e9a3f80a0c576472a7726532317dc4dea88
larissaGimenez <laris.gimenez13@gmail.com>
2024-08-12 23:24:58 -0300

    Ajustes no tamanho do Banner, inclusão de da fonte Poppins e css do título do site

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,10 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     
@@ -41,7 +45,9 @@
         </div>
     </header>
         
-    <section class="banner">descubra</section>
+    <section class="banner">
+        <h1>DESCUBRA O NOVO RH</h1>
+    </section>
 
     <!-- Services Section -->
     <section id="services" class="text-center py-5">
